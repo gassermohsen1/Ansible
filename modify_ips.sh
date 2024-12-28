69dc2073a9fa5ef28c3d501b42c2bd8414849959
gassermohsen1 <gassermohsen83@hotmail.com>
2024-12-28 02:51:53 +0200

    Update modify_ips.sh

--- modify_ips.sh
+++ modify_ips.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Define the paths to the files containing the IPs
-master_ip_file="~/Ansible/masternode_ip"
-worker_ip_file="~/Ansible/workernode_ip"
+master_ip_file="$HOME/Ansible/masternode_ip"
+worker_ip_file="$HOME/Ansible/workernode_ip"
 
 # Read the IPs from the files
-masternode_ip=$(cat $master_ip_file)
-workernode_ip=$(cat $worker_ip_file)
+masternode_ip=$(cat "$master_ip_file")
+workernode_ip=$(cat "$worker_ip_file")
 
 # Define the path to your inventory file
-inventory_file="inventory.txt"
+inventory_file="$HOME/Ansible/inventory.txt"
 
 # Replace the placeholders in the inventory file with the actual IPs
-sed -i "s/masternode_ip/$masternode_ip/" $inventory_file
-sed -i "s/workernode_ip/$workernode_ip/" $inventory_file
+sed -i "s/masternode_ip/$masternode_ip/" "$inventory_file"
+sed -i "s/workernode_ip/$workernode_ip/" "$inventory_file"
 
 echo "Inventory file updated with the correct IPs."
