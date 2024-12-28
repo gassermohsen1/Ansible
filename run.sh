359aa71737bef73dbc6fdb5def262c238425328c
gassermohsen1 <gassermohsen83@hotmail.com>
2024-12-28 11:21:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod 600 $HOME/connect_key.pem
-ansible-playbook -i inventory.txt $HOME/Ansible/playbooks/kube-dependencies.yaml
-ansible-playbook -i inventory.txt $HOME/Ansible/playbooks/master.yaml
-ansible-playbook -i inventory.txt $HOME/Ansible/playbooks/worker.yaml
-ansible-playbook -i inventory.txt $HOME/Ansible/playbooks/juice-shop.yaml
+ansible-playbook -i Ansible/inventory.txt $HOME/Ansible/playbooks/kube-dependencies.yaml
+ansible-playbook -i Ansible/inventory.txt $HOME/Ansible/playbooks/master.yaml
+ansible-playbook -i Ansible/inventory.txt $HOME/Ansible/playbooks/worker.yaml
+ansible-playbook -i Ansible/inventory.txt $HOME/Ansible/playbooks/juice-shop.yaml
