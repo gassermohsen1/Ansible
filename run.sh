d5a59c3b7a2505962e92df084f5a65920bdac53d
gassermohsen1 <gassermohsen83@hotmail.com>
2024-12-28 12:04:30 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,9 @@ chmod 600 $HOME/connect_key.pem
 ansible-playbook -i Ansible/inventory.txt $HOME/Ansible/playbooks/kube-dependencies.yaml
 ansible-playbook -i Ansible/inventory.txt $HOME/Ansible/playbooks/master.yaml
 ansible-playbook -i Ansible/inventory.txt $HOME/Ansible/playbooks/worker.yaml
+
+#wait for 1 minute to let the ingress controller ready 
+
+sleep 60
+
 ansible-playbook -i Ansible/inventory.txt $HOME/Ansible/playbooks/juice-shop.yaml
